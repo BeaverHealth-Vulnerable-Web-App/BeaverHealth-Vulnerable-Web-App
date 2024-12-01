54a36c15888c90a2db4a1f2eee3e335d04d7ba32
Cody Ray <cody.alx.ray@proton.me>
2024-12-01 12:59:35 -0800

    Add helper functions to patient model

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -4,29 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Patient extends Model
 {
     use HasFactory;
 
     protected $table = 'patient';
+
     protected $primaryKey = 'patient_id';
 
     protected $fillable = [
         'first_name',
         'last_name',
         'date_of_birth',
-        'ssn',
         'policy_number',
         'address',
         'is_employee',
     ];
 
     /**
-     * Relationship to PatientFeedback.
+     * Get the feedback for the patient.
      */
     public function feedback()
     {
         return $this->hasMany(PatientFeedback::class, 'patient_id', 'patient_id');
+    }
+
+    /**
+     * Accessor for first_name to return capitalized.
+     */
+    public function getFirstNameAttribute($value)
+    {
+        return Str::title(strtolower($value));
+    }
+
+    /**
+     * Accessor for last_name to return capitalized.
+     */
+    public function getLastNameAttribute($value)
+    {
+        return Str::title(strtolower($value));
     }
 }
