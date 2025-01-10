66656d9c04ec9d4cf47fd5c4349c4cb984117312
Cody Ray <cody.alx.ray@proton.me>
2025-01-09 20:21:52 -0800

    Refactor postbuild.js

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,56 +2,47 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-async function moveManifest() {
-  // Resolve paths based on the project root
+// Guarentee manifest.json is in public/build
+// Sometimes manifest.json is created in public/build/.vite, which breaks the app
+async function guarenteeManifestLocation() {
   const projectRoot = path.resolve(__dirname, '..');
   const buildDir = path.join(projectRoot, 'public', 'build');
   const viteDir = path.join(buildDir, '.vite');
 
+  // Check if manifest.json already exists in public/build
   try {
-    // Check if manifest.json exists in buildDir
     await fs.access(path.join(buildDir, 'manifest.json'));
-    console.log(`Found manifest.json in ${buildDir}`);
     process.exit(0);
-  } catch (err) {
-    // manifest.json not found in buildDir
-    console.log(`Failed to locate manifest.json in ${buildDir}`);
   }
 
+  // Check if the .vite directory exists
   try {
-    // Check if the .vite directory exists
     await fs.access(viteDir);
-    console.log(`Vite directory exists: ${viteDir}`);
   } catch (err) {
-    console.error(`Failed to locate manifest.json`);
-    process.exit(1); // Exit with error
+    // manifest.json is not in public/build and there is no .vite directory
+    process.exit(1);
   }
 
+  // Move all files in public/build/.vite to public/build
   try {
-    // Move files from .vite to buildDir
     const files = await fs.readdir(viteDir);
-    console.log(`Moving build output files: ${files.join(', ')}`);
 
     await Promise.all(
       files.map(async (file) => {
         const src = path.join(viteDir, file);
         const dest = path.join(buildDir, file);
-        console.log(`Moving ${src} to ${dest}`);
         await fs.rename(src, dest);
       })
     );
 
-    console.log(`Successfully moved files from ${viteDir} to ${buildDir}`);
     await fs.rmdir(viteDir);
-    console.log(`Removed directory: ${viteDir}`);
   } catch (err) {
-    console.error('Error moving files:', err);
+    console.error('Error moving files from public/build/.vite to public/build:', err);
     process.exit(1);
   }
 }
 
-moveManifest();
+guarenteeManifestLocation();
