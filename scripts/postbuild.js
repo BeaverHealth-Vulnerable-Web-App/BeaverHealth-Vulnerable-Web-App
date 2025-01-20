827fb5d5ab3c155a6c460c8d18f288a2d888aa61
Cody Ray <cody.alx.ray@proton.me>
2025-01-19 16:50:45 -0800

    postbuild: use fs.existsSync and make code self-documenting

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,53 +1,43 @@
-import { promises as fs } from 'fs';
+import fs, { promises as fsPromises } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Make sure manifest.json is in public/build
-// Sometimes manifest.json is created in public/build/.vite, which breaks the app
+// Ensures manifest.json is in expected location (public/build)
 async function ensureManifestLocation() {
   const projectRoot = path.resolve(__dirname, '..');
   const buildDir = path.join(projectRoot, 'public', 'build');
   const viteDir = path.join(buildDir, '.vite');
   const manifestPath = path.join(buildDir, 'manifest.json');
 
-  // Exit if manifest.json already exists in public/build
-  if (await fileExists(manifestPath)) {
+  if (fs.existsSync(manifestPath)) {
+    console.log('manifest.json is already in public/build');
     process.exit(0);
   }
 
-  // Exit with error if .vite directory is missing
-  // This means manifest.json can't be found
-  if(!(await fileExists(viteDir))) {
+  if (!fs.existsSync(viteDir)) {
+    console.error('.vite directory is missing. manifest.json could not be located');
     process.exit(1);
   }
 
-  // Move files from public/build/.vite to public/build
   try {
-    const files = await fs.readdir(viteDir);
+    const files = await fsPromises.readdir(viteDir);
 
     await Promise.all(
       files.map(async (file) => {
         const src = path.join(viteDir, file);
         const dst = path.join(buildDir, file);
-        await fs.rename(src, dst);
+        await fsPromises.rename(src, dst);
       })
     );
 
-    await fs.rmdir(viteDir);
-  } catch {
+    await fsPromises.rmdir(viteDir);
+    console.log('Moved manifest.json from public/build/.vite to public/build');
+  } catch (error) {
+    console.error('Encountered an error while moving files from public/build/.vite to public/build:', error.message);
     process.exit(1);
-  }
-}
-
-async function fileExists(filePath) {
-  try {
-    await fs.stat(filePath);
-    return true;
-  } catch {
-    return false;
   }
 }
 
