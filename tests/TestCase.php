834791842171f9ad4ccecf9b2499273b5e4eeeb5
Cody Ray <cody.alx.ray@proton.me>
2025-01-27 23:32:15 -0800

    Add postWithCsrf method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function postWithCsrf(string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->post($route, array_merge(['_token' => session()->token()], $data));
+    }
 }
