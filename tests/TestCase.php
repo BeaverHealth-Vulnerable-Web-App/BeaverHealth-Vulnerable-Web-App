38133d605ecbcd12132c078f46d1a970ac830762
Cody Ray <cody.alx.ray@proton.me>
2025-01-28 18:13:22 -0800

    Configure TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use DatabaseTransactions;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
     protected function postWithCsrf(string $route, array $data = []): \Illuminate\Testing\TestResponse
     {
         return $this->post($route, array_merge(['_token' => session()->token()], $data));
