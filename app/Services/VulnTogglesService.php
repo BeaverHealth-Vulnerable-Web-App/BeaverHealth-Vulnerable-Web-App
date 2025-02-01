960be56f60e8b6952db3346d243f1d852f78f547
Sean Brady <swangeon@gmail.com>
2025-01-31 19:25:21 -0600

    refactor: General refactor for readability
    Add an error message for a invalid toggle name when that case arises
    Refactor the `auth()->user()` calls into just `$user` for readabilty

--- app/Services/VulnTogglesService.php
+++ app/Services/VulnTogglesService.php
@@ -8,14 +8,15 @@ class VulnTogglesService
 {
     public function updateVulnToggles(VulnTogglesRequest $request)
     {
+        $user = auth()->user();
         $toggle = $request->input('toggle');
         $value = $request->input('value') ? true : false;
-        if (array_key_exists($toggle, auth()->user()->getAttributes())) {
-            auth()->user()->{$toggle} = $value;
-            auth()->user()->save();
+        if (array_key_exists($toggle, $user->getAttributes())) {
+            $user->update([$toggle => $value]);
+            $user->save();
             return response()->json(['success' => true]);
         } else {
-            return response()->json(['success' => false]);
+            return response()->json(['success' => false, 'error' => 'Invalid toggle name'], 422);
         }
     }
 }
