e895c360e587542e12268e281efc792548934b0b
Sean Brady <swangeon@gmail.com>
2025-01-31 18:43:43 -0600

    testing: Add macro for the class to assert that a CSRF Mismatch error occured

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,23 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        TestResponse::macro(
+            'assertCsrfMismatch',
+            function () {
+                return $this->assertStatus(419);
+            }
+        );
+    }
 
     protected function postWithCsrf(string $route, array $data = []): \Illuminate\Testing\TestResponse
     {
