465e78fe23b0e5b42c568d90dc808f7a2b334743
Cody Ray <cody.alx.ray@proton.me>
2025-02-08 19:48:54 -0800

    Use anonymous migration classes

--- database/migrations/2024_11_19_154054_create_patient_table.php
+++ database/migrations/2024_11_19_154054_create_patient_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePatientTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -36,4 +36,4 @@ class CreatePatientTable extends Migration
     {
         Schema::dropIfExists('patient');
     }
-}
+};
