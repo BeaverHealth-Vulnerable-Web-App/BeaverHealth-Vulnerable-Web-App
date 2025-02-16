cdf1aaac461a8825239de8ca9ba6e7385d28ace2
Alexa <herreraalexa@icloud.com>
2025-02-16 11:03:07 -0800

    Update routes to fix code sniffer warning

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -6,39 +6,40 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Ensures manifest.json is in expected location (public/build)
-async function ensureManifestLocation() {
-  const projectRoot = path.resolve(__dirname, '..');
-  const buildDir = path.join(projectRoot, 'public', 'build');
-  const viteDir = path.join(buildDir, '.vite');
-  const manifestPath = path.join(buildDir, 'manifest.json');
+async function ensureManifestLocation()
+{
+    const projectRoot = path.resolve(__dirname, '..');
+    const buildDir = path.join(projectRoot, 'public', 'build');
+    const viteDir = path.join(buildDir, '.vite');
+    const manifestPath = path.join(buildDir, 'manifest.json');
 
-  if (fs.existsSync(manifestPath)) {
-    console.log('manifest.json is already in public/build');
-    process.exit(0);
-  }
+    if (fs.existsSync(manifestPath)) {
+        console.log('manifest.json is already in public/build');
+        process.exit(0);
+    }
 
-  if (!fs.existsSync(viteDir)) {
-    console.error('.vite directory is missing. manifest.json could not be located');
-    process.exit(1);
-  }
+    if (!fs.existsSync(viteDir)) {
+        console.error('.vite directory is missing. manifest.json could not be located');
+        process.exit(1);
+    }
 
-  try {
-    const files = await fsPromises.readdir(viteDir);
+    try {
+        const files = await fsPromises.readdir(viteDir);
 
-    await Promise.all(
-      files.map(async (file) => {
-        const src = path.join(viteDir, file);
-        const dst = path.join(buildDir, file);
-        await fsPromises.rename(src, dst);
-      })
-    );
+        await Promise.all(
+            files.map(async(file) => {
+                const src = path.join(viteDir, file);
+                const dst = path.join(buildDir, file);
+                await fsPromises.rename(src, dst);
+            })
+        );
 
-    await fsPromises.rmdir(viteDir);
-    console.log('Moved manifest.json from public/build/.vite to public/build');
-  } catch (error) {
-    console.error('Encountered an error while moving files from public/build/.vite to public/build:', error.message);
-    process.exit(1);
-  }
+        await fsPromises.rmdir(viteDir);
+        console.log('Moved manifest.json from public/build/.vite to public/build');
+    } catch (error) {
+        console.error('Encountered an error while moving files from public/build/.vite to public/build:', error.message);
+        process.exit(1);
+    }
 }
 
 ensureManifestLocation();
