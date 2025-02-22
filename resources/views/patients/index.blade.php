996e8d542a4d6cca57d23e4e40ab73ad088c2348
Cody Ray <cody.alx.ray@proton.me>
2025-02-22 10:26:36 -0800

    Patient info dark mode

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -7,27 +7,36 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="table-auto w-full">
-                        <thead>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+                    <table class="table-auto w-full border border-gray-200 dark:border-gray-700">
+                        <thead class="bg-gray-100 dark:bg-gray-700">
                             <tr>
-                                <th class="px-4 py-2">Name</th>
-                                <th class="px-4 py-2">Date of Birth</th>
-                                <th class="px-4 py-2">Policy Number</th>
-                                <th class="px-4 py-2">Address</th>
-                                <th class="px-4 py-2">Actions</th>
+                                <th class="px-4 py-2 text-gray-700 dark:text-gray-300 border dark:border-gray-600">Name</th>
+                                <th class="px-4 py-2 text-gray-700 dark:text-gray-300 border dark:border-gray-600">Date of Birth</th>
+                                <th class="px-4 py-2 text-gray-700 dark:text-gray-300 border dark:border-gray-600">Policy Number</th>
+                                <th class="px-4 py-2 text-gray-700 dark:text-gray-300 border dark:border-gray-600">Address</th>
+                                <th class="px-4 py-2 text-gray-700 dark:text-gray-300 border dark:border-gray-600">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($patients as $patient)
-                                <tr>
-                                    <td class="border px-4 py-2">{{ $patient->first_name }} {{ $patient->last_name }}</td>
-                                    <td class="border px-4 py-2">{{ $patient->date_of_birth }}</td>
-                                    <td class="border px-4 py-2">{{ $patient->policy_number }}</td>
-                                    <td class="border px-4 py-2">{{ $patient->address }}</td>
-                                    <td class="border px-4 py-2">
-                                        <a href="{{ route('patients.info', ['id' => $patient->patient_id]) }}" class="text-blue-500 hover:underline">
+                                <tr class="bg-white dark:bg-gray-900 even:bg-gray-50 dark:even:bg-gray-800">
+                                    <td class="border px-4 py-2 text-gray-800 dark:text-gray-300 border-gray-200 dark:border-gray-700">
+                                        {{ $patient->first_name }} {{ $patient->last_name }}
+                                    </td>
+                                    <td class="border px-4 py-2 text-gray-800 dark:text-gray-300 border-gray-200 dark:border-gray-700">
+                                        {{ $patient->date_of_birth }}
+                                    </td>
+                                    <td class="border px-4 py-2 text-gray-800 dark:text-gray-300 border-gray-200 dark:border-gray-700">
+                                        {{ $patient->policy_number }}
+                                    </td>
+                                    <td class="border px-4 py-2 text-gray-800 dark:text-gray-300 border-gray-200 dark:border-gray-700">
+                                        {{ $patient->address }}
+                                    </td>
+                                    <td class="border px-4 py-2 border-gray-200 dark:border-gray-700">
+                                        <a href="{{ route('patients.info', ['id' => $patient->patient_id]) }}"
+                                           class="text-blue-500 dark:text-blue-400 hover:underline">
                                             View Details
                                         </a>
                                     </td>
