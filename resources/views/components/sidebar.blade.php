79839a5710854287a91f53326cbcdf6379ec1153
Cody Ray <cody.alx.ray@proton.me>
2025-02-22 10:26:12 -0800

    Re-order sidebar links

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,9 +9,9 @@
     <!-- Navigation Links -->
     <div class="flex-grow">
         <nav class="space-y-4">
-            <!-- Records Request Link -->
-            @if(auth()->user()->request_records)
-                <a href="{{ route('records.request') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Request Records</a>
+            <!-- Admin Link -->
+            @if(auth()->user()->is_admin)
+                <a href="{{ route('admin') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Admin</a>
             @endif
 
             <!-- Records Add Link -->
@@ -19,13 +19,13 @@
                 <a href="{{ route('records.add') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Add Records</a>
             @endif
 
+            <!-- Records Request Link -->
+            @if(auth()->user()->request_records)
+                <a href="{{ route('records.request') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Request Records</a>
+            @endif
+
             <!-- Feedback Link -->
             <a href="{{ route('feedback') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Patient Feedback</a>
-
-            <!-- Admin Link -->
-            @if(auth()->user()->is_admin)
-                <a href="{{ route('admin') }}" class="block py-2 px-4 rounded hover:bg-gray-600">Admin</a>
-            @endif
 
             <!-- Patient Information Link -->
             @if(auth()->user()->view_patient_info)
