bd5b33c37446891dc4eeac497e4932ca7ea0ad71
Cody Ray <cody.alx.ray@proton.me>
2025-02-22 13:17:38 -0800

    Add fade out to feedback success message

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -18,8 +18,11 @@
             setTimeout(() => {
                 let message = document.getElementById('success-message');
                 if (message) {
+                    message.style.transition = 'opacity 0.5s ease-in-out';
                     message.style.opacity = '0';
-                    setTimeout(() => message.remove(), 500);
+                    setTimeout(() => {
+                        message.remove();
+                    }, 500);
                 }
             }, 3000);
         </script>
