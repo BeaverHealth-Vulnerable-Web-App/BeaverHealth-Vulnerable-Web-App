57cc466dc62e7d20ed889baca43188910934c4ef
Cody Ray <cody.alx.ray@proton.me>
2025-02-25 17:27:32 -0800

    Increase status message size

--- resources/views/components/status-message.blade.php
+++ resources/views/components/status-message.blade.php
@@ -1,6 +1,6 @@
 <div
     id="status-message"
-    class="absolute top-0 right-0 mt-2 mr-2 p-2 rounded text-sm
+    class="absolute top-0 right-0 mt-2 mr-2 p-2 rounded text-base
         {{ $type == 'error' ?
             'bg-red-100 dark:bg-red-900 border border-red-200 dark:border-red-700 text-red-800 dark:text-red-300' :
             'bg-green-100 dark:bg-green-900 border border-green-200 dark:border-green-700 text-green-800 dark:text-green-300'
