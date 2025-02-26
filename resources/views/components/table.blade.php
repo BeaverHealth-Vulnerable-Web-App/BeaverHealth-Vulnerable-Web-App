5ab8d33d94e2880b1d689072353600589228405b
Cody Ray <cody.alx.ray@proton.me>
2025-02-25 20:50:56 -0800

    Fix table border issue

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,9 +1,9 @@
-<div class="rounded-lg overflow-hidden border border-gray-300 dark:border-gray-700">
+<div class="rounded-lg overflow-hidden border border-transparent dark:border-gray-700">
     <table class="table-auto w-full">
         <thead>
             <tr class="bg-gray-700 text-white">
                 @foreach($headers as $header)
-                    <th class="px-4 py-3 text-{{ $header['align'] ?? 'left' }} border-b border-gray-300 dark:border-gray-700">
+                    <th class="px-4 py-3 text-{{ $header['align'] ?? 'left' }} border-b border-transparent dark:border-gray-700">
                         {{ $header['text'] }}
                     </th>
                 @endforeach
