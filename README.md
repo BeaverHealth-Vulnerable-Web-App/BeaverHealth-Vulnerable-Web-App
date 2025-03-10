bf78d0f024657cb4f734ea543b9c4ded593defad
Sean Brady <swangeon@gmail.com>
2025-03-09 21:22:38 -0500

    rewrite: Rewrite things to be more concise on the Dashboard and move disclaimer to README.

--- README.md
+++ README.md
@@ -4,11 +4,17 @@
 
 1. Install dependencies
 Make sure you have Docker and Docker Compose available on your system:
-- [Docker](https://www.docker.com/get-started/)
-- [Docker Compose](https://docs.docker.com/compose/install/) (if not included with Docker)
+
+    - [Docker](https://www.docker.com/get-started/)
+    - [Docker Compose](https://docs.docker.com/compose/install/) (if not included with Docker)
+
 2. Clone the repo 🌀
 3. Navigate to the project root 🫚  
   `cd ./BeaverHealth-Vulnerable-Web-App`
 4. Run the setup script
   `./setup.sh --fresh`
 5. In a web browser, navigate to `localhost:9991` 🗺️  
+
+## Disclaimer
+
+This application is intended for educational and testing purposes only, and we do not take responsibility for any misuse. Do not publicly expose this application to the internet on any machine you control, as it contains vulnerabilities that are not intended to be used in a production environment or exposed to the internet. It is recommend using a virtual machine (such as VirtualBox or VMWare) and setting up the virtual machine to NAT networking mode for the best security of your host machine when running the application.
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,14 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="flex justify-center">
-                        <h1 class="text-2xl font-bold text-gray-900 dark:text-gray-100 content-center">Welcome to the BeaverHealth Vulnerable Web Application Dashboard</h1>
+                        <h1 class="text-2xl font-bold text-gray-900 dark:text-gray-100 content-center">Welcome to the BeaverHealth Vulnerable Web Application</h1>
                     </div>
                     <br>
-                    <p>The BeaverHealth Vulnerable Web Application is a deliberately vulnerable PHP Laravel/MySQL web application designed to educate users about common web security vulnerabilities from the OWASP Top 10. It provides a safe, controlled environment with comprehensive documentation on each vulnerability, including what they are, how to exploit them, and how to fix them to help security researchers, developers, and students learn through hands-on experience.</p>
-                    <br>
-                    <h2 class="text-xl font-bold">WARNING!</h2>
-                    <br>
-                    <p>This application is intended for educational and testing purposes only, and we do not take responsibility for any misuse. <em>Do not publically expose this application to the internet on any machine you control, as it contains vulnerabilities that are not intended to be used in a production environment or exposed to the internet.</em> It is recommend using a virtual machine (such as VirtualBox or VMWare) and setting up the virtual machine to NAT networking mode for the best security of your host machine when running the application.</p>
+                    <p>BeaverHealth is a deliberately vulnerable PHP Laravel/MySQL web application designed to educate users about a subset of common web security vulnerabilities from the OWASP Top 10. It provides a safe, controlled environment with comprehensive documentation on what each vulnerability is, how to exploit them, and how to fix them. Our goal with this project is to help security researchers, developers, and students build a deeper understanding of web application security through hands-on experience.</p>
                     <br>
                     <h2 class="text-xl font-bold">Links</h2>
                     <br>
