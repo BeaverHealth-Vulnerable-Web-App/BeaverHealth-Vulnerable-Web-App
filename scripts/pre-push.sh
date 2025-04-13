8c8c08fd4027af16e33d582a7244388e77617a29
Cody Ray <cody.alx.ray@proton.me>
2025-04-12 17:10:57 -0700

    Fix APP_KEY issue

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -5,15 +5,14 @@ set -e
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$DIR/.."
 
-if [[ ! -f .env ]]; then
+if [ ! -f .env ]; then
     echo "No .env found. Copying from .env.example..."
     cp .env.example .env
-
-    echo "Generating APP_KEY..."
-    key=$(./vendor/bin/sail artisan key:generate --show)
-    sed -i "s|^APP_KEY=.*|APP_KEY=${key}|" .env
 fi
 
-./vendor/bin/sail up -d
+echo "Generating fresh APP_KEY..."
+key=$(./vendor/bin/sail artisan key:generate --show)
+sed -i "s|^APP_KEY=.*|APP_KEY=${key}|" .env
 
+./vendor/bin/sail up -d
 ./vendor/bin/sail artisan test
