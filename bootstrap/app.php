f6763fba7501304af51e3cf2f172b3ca1fd6fea4
Cody Ray <cody.alx.ray@proton.me>
2025-04-13 15:03:40 -0700

    Logging + some refactoring to make logging cleaner

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,13 @@
 <?php
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Middleware\CheckPermissionMiddleware;
+use App\Http\Middleware\LogUserActivityMiddleware;
+use App\Services\UserActivityLogger;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(
         function (Middleware $middleware) {
+            $middleware->use([
+                LogUserActivityMiddleware::class,
+            ]);
             $middleware->alias([
                 'check.permission' => CheckPermissionMiddleware::class,
             ]);
@@ -19,6 +26,17 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withExceptions(
         function (Exceptions $exceptions) {
-            //
+            $exceptions->renderable(
+                function (NotFoundHttpException $e) {
+                    $logger = app(UserActivityLogger::class);
+                    if (Auth::check()) {
+                        $logger->info('User accessed unregistered route');
+                        return redirect()->route('dashboard');
+                    } else {
+                        $logger->warning('Guest accessed unregistered route');
+                        return redirect()->route('login');
+                    }
+                }
+            );
         }
     )->create();
